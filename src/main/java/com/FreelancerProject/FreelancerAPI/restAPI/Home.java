bb4042fa17b3b4910febcb873a3177bcb6f48a73
Burak Gülsoy <75689293+nebilim4@users.noreply.github.com>
2022-08-19 11:37:25 +0300

    starting point for jpa authentication

---
+++ src/main/java/com/FreelancerProject/FreelancerAPI/restAPI/Home.java
@@ -0,0 +1,13 @@
+package com.FreelancerProject.FreelancerAPI.restAPI;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Home {
+
+    @GetMapping("/")
+    public String home() {
+        return ("This is the homepage");
+    }
+}
--- src/main/java/com/FreelancerProject/FreelancerAPI/security/SecurityConfiguration.java
+++ src/main/java/com/FreelancerProject/FreelancerAPI/security/SecurityConfiguration.java
@@ -1,13 +1,18 @@
-package com.FreelancerProject.FreelancerAPI;
+package com.FreelancerProject.FreelancerAPI.security;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
 
 @EnableWebSecurity
 @Configuration
@@ -37,6 +42,20 @@ public class SecurityConfiguration {
     @Autowired
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+//        http.authorizeHttpRequests((authz) -> authz.anyRequest()
+//                .authenticated())
+//                .httpBasic(withDefaults());
+        http.authorizeHttpRequests()
+                .antMatchers("/").permitAll()
+                .and().formLogin();
+
+
+        return http.build();
     }
 
 
